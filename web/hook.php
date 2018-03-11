744e9d376014459f033315075dad8d9c8ff0e9a0
Demkin Denis <denzzeuro@gmail.com>
2018-03-11 14:02:05 +0400

    продолжаем эксперименты

--- web/hook.php
+++ web/hook.php
@@ -69,7 +69,8 @@ try {
         foreach(['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е'] as $letter) {
             $letter_array[stripos($text, $letter)] = $letter;
         }
-        $text = 'Седых'.mb_stristr($text, reset($letter_array));
+        $pos_letter = reset($letter_array);
+        $text = 'Седых'.($pos_letter ? mb_stristr($text, $pos_letter) : '');
         // if (stripos($text, 'у') !== false) {
         //     $text = 'Седых'.mb_stristr($text, 'у');
         // }
