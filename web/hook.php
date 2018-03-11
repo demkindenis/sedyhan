525a51bc26874582e39aa8dc119398695d7c1b40
Demkin Denis <denzzeuro@gmail.com>
2018-03-11 13:06:18 +0400

    test

--- web/hook.php
+++ web/hook.php
@@ -16,9 +16,25 @@ use Longman\TelegramBot\Entities\Update;
 require_once '../vendor/autoload.php';
 require_once 'config.php';
 
+class MyTelegram extends Longman\TelegramBot\Telegram {
+    public function processUpdate(Longman\TelegramBot\Entities\Update $update) {
+      if($message = $update->getMessage()) {
+        $sender = $message->getFrom();
+        $sender_id = $sender->getId();
+        $sender_username = $sender->getUsername();
+        $chat = $message->getChat();
+        $chat_id = $chat->getId();
+        $message_text = $message->getText();
+        // etc etc ....
+      }
+  
+      return parent::processUpdate($update);
+    }
+  }
+
 try {
     // Create Telegram API object
-    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+    $telegram = new MyTelegram($bot_api_key, $bot_username);
 
     // Add commands paths containing your custom commands
     $telegram->addCommandsPaths($commands_paths);
@@ -59,7 +75,7 @@ try {
     // $text = $telegram ->getMessage()->getText(true);
 
     // Request::sendMessage(['chat_id' => '533910', 'text' => serialize($telegram ->getMessage())]);
-    Request::sendMessage(['chat_id' => '533910', 'text' => Update::getMessage()]);
+    Request::sendMessage(['chat_id' => '533910', 'text' => $telegram->message]);
 
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
     // Silence is golden!
