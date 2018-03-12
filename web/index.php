2fafdf62d35673208b8b912721c551fb6152c1db
Demkin Denis <denzzeuro@gmail.com>
2018-03-12 17:40:40 +0400

    fix

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Седых спизди</title>
-  <script   src="https://code.jquery.com/jquery-3.3.1.slim.min.js"   integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="   crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 </head>
 <body>
   <select id="chat" name="chat">
@@ -18,7 +18,8 @@
     $(document)
       .on('click', '#say', function() {
         var text = encodeURIComponent($('#text').val());
-        $.post('https://api.telegram.org/bot566295728:AAFrqfGoM1P7FQxlEs2cjHbX9V747bIgH_k/sendMessage?chat_id='+$('#chat').val()+'&parse_mode=html&text=123', function() {
+        var url  = 'https://api.telegram.org/bot566295728:AAFrqfGoM1P7FQxlEs2cjHbX9V747bIgH_k/sendMessage?chat_id='+$('#chat').val()+'&parse_mode=html&text='+text;
+        $.post(url, function() {
           alert('я пизданул');
         });
       })
