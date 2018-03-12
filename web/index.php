cfab8a7844cd0df9b718e34053079a593b78033e
Demkin Denis <denzzeuro@gmail.com>
2018-03-12 17:16:26 +0400

    накидал отправку текста ботом

--- web/index.php
+++ web/index.php
@@ -1,25 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Седых спизди</title>
+  <script   src="https://code.jquery.com/jquery-3.3.1.slim.min.js"   integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="   crossorigin="anonymous"></script>
+</head>
+<body>
+  <select id="chat" name="chat">
+    <option value="533910">Денис</option>
+    <option value="-1001149670525">Грекос (Grekos)</option>
+  </select>
+  Седых спизди <input id="text" type="text" name="text"><span id="say">Пиздануть</span>
 
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+  <script>
+    $(document)
+      .on('click', '#say', function() {
+        var chat_id = '533910';
+        var text = 'testtest';
+        
+        $.post('https://api.telegram.org/bot566295728:AAFrqfGoM1P7FQxlEs2cjHbX9V747bIgH_k/sendMessage?chat_id='+$('#chat option:selected').val()+'&parse_mode=html&text='+encodeURIComponent($('#text')), function() {
+          alert('я пизданул');
+        });
+      })
+  </script>
+</body>
+</html>
